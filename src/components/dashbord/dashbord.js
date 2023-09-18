1e6cfb993717bdadbd32d4cde9eb60a102fa9817
rifaqueahammed <rifaqueahammed@gmail.com>
2023-09-18 21:44:36 +0530

    second commit

--- src/components/dashbord/dashbord.js
+++ src/components/dashbord/dashbord.js
@@ -1,74 +1,138 @@
-import React from 'react'
-import './dashboard.css'
+import React from "react";
+import "./dashboard.css";
 
 function Dashbord() {
-
   const plans = [
-    {name:"Basic", rate:89.99, offerRate:9.99, users:25, storage:25,color:'#deb046'},
-    {name:"Standard", rate:189.99, offerRate:99.99, users:50, storage:60,color:'#f06767'},
-    {name:"Premium", rate:389.99, offerRate:199.99, users:75, storage:100,color:'#eb44d2'},
-]
+    {
+      name: "Basic",
+      rate: 89.99,
+      offerRate: 9.99,
+      users: 25,
+      storage: 25,
+      color: "#deb046",
+    },
+    {
+      name: "Standard",
+      rate: 189.99,
+      offerRate: 99.99,
+      users: 50,
+      storage: 60,
+      color: "#f06767",
+    },
+    {
+      name: "Premium",
+      rate: 389.99,
+      offerRate: 199.99,
+      users: 75,
+      storage: 100,
+      color: "#eb44d2",
+    },
+  ];
 
-  
+  const data = [
+    {
+      name: "Free Starter",
+      tag: "Free Forever",
+      description: "Hello this is free,you can enjoy our free tier",
+      users: 8,
+      storage: 3,
+      color: "#92ed7c",
+    },
+    {
+      name: "Enterprise Plan",
+      tag: "Lets Connect",
+      description: "Build you enterprise with our team",
+      users: 75,
+      storage: "unlimited",
+      color: "#68c7e6",
+    },
+  ];
+
   return (
-    <div className='dashboard-main'>
+    <div className="dashboard-main">
       <h2>Choose a plan that just rigth for you! </h2>
-      <div className='dashboard-section1'>
-        {plans.map((item,key)=>(
-          <div className='card1' key={key}>
-          <h3 >{item.name}</h3>
-          <p className='strikeP'>$ {item.rate}/mo</p>
-          <h3>$ {item.offerRate}/mo</h3>
-          <button style={{backgroundColor:item.color, border:'none', padding:'5px'}}>Get Started <i class="fa-solid fa-arrow-right"></i></button>
-          <hr/>
-          <p>What you will get</p>
-          <div className='plan-feutures-main'>
-            <div className='plan-feutures'><i class="fa-solid fa-user"></i><p>Up to {item.users} Users</p></div>
-            <div className='plan-feutures'><i class="fa-solid fa-cloud"></i><p>Up to {item.storage}gb storage</p></div>
-            <div className='plan-feutures'><i class="fa-solid fa-envelope"></i><p>Email Support</p></div>
+      <div className="dashboard-section1">
+        {plans.map((item, key) => (
+          <div className="card1" key={key}>
+            <h3>{item.name}</h3>
+            <p className="strikeP">$ {item.rate}/mo</p>
+            <h3>$ {item.offerRate}/mo</h3>
+            <button
+              style={{
+                backgroundColor: item.color,
+                border: "none",
+                padding: "5px",
+              }}
+            >
+              Get Started <i class="fa-solid fa-arrow-right"></i>
+            </button>
+            <hr />
+            <p>What you will get</p>
+            <div className="plan-feutures-main">
+              <div className="plan-feutures">
+                <i className="fa-solid fa-user"></i>
+                <p>Up to {item.users} Users</p>
+              </div>
+              <div className="plan-feutures">
+                <i className="fa-solid fa-cloud"></i>
+                <p>Up to {item.storage}gb storage</p>
+              </div>
+              <div className="plan-feutures">
+                <i className="fa-solid fa-envelope"></i>
+                <p>Email Support</p>
+              </div>
+            </div>
           </div>
-       </div>
-
         ))}
-       
-       
+      </div>
+      <div className="dashboard-section2">
+        {data.map((item, key) => (
+          <div className="card2" key={key}>
+            <div>
+              <p
+                style={{
+                  backgroundColor: item.color,
+                  border: "none",
+                  padding: "2px",
+                  width: "50%",
+                }}
+              >
+                {item.tag}
+              </p>
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <button
+                style={{
+                  backgroundColor: item.color,
+                  border: "none",
+                  padding: "5px",
+                }}
+              >
+                Get Started <i class="fa-solid fa-arrow-right"></i>
+              </button>
+            </div>
+            <div>
+              <p>What you will get:</p>
+              <div className="card2-feautures">
+                <div className="plan-feutures">
+                  <i className="fa-solid fa-user"></i>
+                  <p>Up to {item.users} Users</p>
+                </div>
+                <div className="plan-feutures">
+                  <i className="fa-solid fa-cloud"></i>
+                  <p>Up to {item.storage}gb storage</p>
+                </div>
+                <div className="plan-feutures">
+                  <i className="fa-solid fa-envelope"></i>
+                  <p>Email Support</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-      <div className='dashboard-section2'>
-      <div className='card2'>
-          <div>
-          <p>Lets Connect</p>
-          <h2>Free Starter</h2>
-           <p>sghi;jinjgsbnjigjhjhjnnjijhxjxkvjbf</p>
-           <button>Get Started <i class="fa-solid fa-arrow-right"></i></button>
-          </div>
-          <div>
-          <p>What you will get:</p>
-           <div className='card2-feautures'>
-             <div className='plan-feutures'><i class="fa-solid fa-user"></i><p>Up to 25 Users</p></div>
-             <div className='plan-feutures'><i class="fa-solid fa-cloud"></i><p>Up to 25gb storage</p></div>
-             <div className='plan-feutures'><i class="fa-solid fa-envelope"></i><p>Email Support</p></div>
-          </div>
-          </div>
-        </div>
-        <div className='card2'>
-          <div>
-          <p>Lets Connect</p>
-          <h2>Free Starter</h2>
-           <p>sghi;jinjgsbnjigjhjhjnnjijhxjxkvjbf</p>
-           <button>Get Started <i class="fa-solid fa-arrow-right"></i></button>
-          </div>
-          <div>
-          <p>What you will get:</p>
-           <div className='card2-feautures'>
-             <div className='plan-feutures'><i class="fa-solid fa-user"></i><p>Up to 25 Users</p></div>
-             <div className='plan-feutures'><i class="fa-solid fa-cloud"></i><p>Up to 25gb storage</p></div>
-             <div className='plan-feutures'><i class="fa-solid fa-envelope"></i><p>Email Support</p></div>
-          </div>
-          </div>
-        </div>
-        </div>
-    </div>
-  )
+  );
 }
 
-export default Dashbord
+export default Dashbord;
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,60 +1,76 @@
-import React from 'react'
-import './home.css';
-import Dashbord from '../dashbord/dashbord';
+import React from "react";
+import "./home.css";
+import Dashbord from "../dashbord/dashbord";
 
 function Home() {
-
   const menuItems = [
-    {name:"Dashboard", component:'Dashboard',icon:"fa-solid fa-table-columns"},
-    {name:"Perks",component:'Perks',icon:"fa-solid fa-hand-holding-heart"},
-    {name:"Addons",component:'Addons',icon:"fa-solid fa-puzzle-piece"},
-    {name:"FAQ",component:'FAQ',icon:"fa-solid fa-clipboard-question"},
-    {name:"Support",component:'Support',icon:"fa-solid fa-comment-dots"}
-]
+    {
+      name: "Dashboard",
+      component: "Dashboard",
+      icon: "fa-solid fa-table-columns",
+    },
+    {
+      name: "Perks",
+      component: "Perks",
+      icon: "fa-solid fa-hand-holding-heart",
+    },
+    { name: "Addons", component: "Addons", icon: "fa-solid fa-puzzle-piece" },
+    { name: "FAQ", component: "FAQ", icon: "fa-solid fa-clipboard-question" },
+    { name: "Support", component: "Support", icon: "fa-solid fa-comment-dots" },
+  ];
 
   return (
-    <div className='main'>
-       <div className='header'>
-          <div className='logo'><h1 className=''>P</h1></div>
-          <div className='dropdown'><span className='dropdown-span'>Hello Rifaque </span> 
-          <select className='dropdown-button'>
+    <div className="main">
+      <div className="header">
+        <div className="logo">
+          <h1 className="">P</h1>
+        </div>
+        <div className="dropdown">
+          <span className="dropdown-span">Hello Rifaque </span>
+          <select className="dropdown-button">
             <option value=""></option>
-             <option value="">Logout</option>
-           </select>
-          </div>
+            <option value="">Logout</option>
+          </select>
+        </div>
       </div>
-       <div className='section' >
-       <div className='nav'>
-        <div className='profile'>
-          <img src='https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1694978049~exp=1694978649~hmac=e2b59807653860ac4e268280582e2f924b5f037df601375fd26ee826449fe5af' alt=''></img>
-           <h3 className='profile-name'>Rifaque</h3>
-           <p>hello@gmail.com</p>
-         </div>
-         <div className='menuItems'>
-            {menuItems.map((menu, key)=>(
-              <div className='menu' key={key}>
+      <div className="section">
+        <div className="nav">
+          <div className="profile">
+            <img
+              src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1694978049~exp=1694978649~hmac=e2b59807653860ac4e268280582e2f924b5f037df601375fd26ee826449fe5af"
+              alt=""
+            ></img>
+            <h3 className="profile-name">Rifaque</h3>
+            <p>hello@gmail.com</p>
+          </div>
+          <div className="menuItems">
+            {menuItems.map((menu, key) => (
+              <div className="menu" key={key}>
                 <i className={` menuicon ${menu.icon}`}></i>
-                <p className='menu-name'>{menu.name}</p>
+                <p className="menu-name">{menu.name}</p>
               </div>
             ))}
-         </div>
-         <div className='logout'>
-                <p>Logout</p>
-                <i className="fa-solid fa-power-off"></i>
-              </div>
-       </div>
-       <div className='section1'>
-          <Dashbord/>
-       </div>
-       <div className='rigth-navbar'>
-            <span className='notification'><i class="fa-solid fa-bell"></i></span>
-            <div className='emptydiv'></div>
-            <span className='plus-icon'><i class="fa-solid fa-plus"></i></span> 
-       </div>
-       </div>
-      
+          </div>
+          <div className="logout">
+            <p>Logout</p>
+            <i className="fa-solid fa-power-off"></i>
+          </div>
+        </div>
+        <div className="section1">
+          <Dashbord />
+        </div>
+        <div className="rigth-navbar">
+          <span className="notification">
+            <i className="fa-solid fa-bell"></i>
+          </span>
+          <div className="emptydiv"></div>
+          <span className="plus-icon">
+            <i className="fa-solid fa-plus"></i>
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
